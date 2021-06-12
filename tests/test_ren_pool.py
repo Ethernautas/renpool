5c631b1a449d4e4467a499a37c985816105533d8
fede-rodes <federodes@gmail.com>
2021-06-12 15:01:03 +0200

    adding deposit test

--- tests/test_ren_pool.py
+++ tests/test_ren_pool.py
@@ -15,14 +15,15 @@ def test_ren_pool_deploy(accounts, ren_pool):
     Test if the contract is correctly deployed.
     """
     assert ren_pool.owner() == accounts[0]
-    assert ren_pool.total_pooled() == 0
+    assert ren_pool.totalPooled() == 0
 
-# def test_ren_pool_deposit(accounts, ren_pool):
-#     """
-#     Test deposit.
-#     """
-#     DEPOSIT = 100
-#     assert ren_pool.total_pooled() == 0
-#     ren_pool.deposit(DEPOSIT, {'from': accounts[0]})
-#     assert ren_pool.balanceOf(accounts[0], {'from': accounts[0]}) == DEPOSIT
-#     assert ren_pool.total_pooled() == DEPOSIT
+def test_ren_pool_deposit(accounts, ren_pool, ren_token):
+    """
+    Test deposit.
+    """
+    DEPOSIT = 100
+    assert ren_pool.totalPooled() == 0
+    ren_token.approve(ren_pool.address, DEPOSIT, {'from': accounts[0]})
+    ren_pool.deposit(DEPOSIT, {'from': accounts[0]})
+    assert ren_pool.balanceOf(accounts[0], {'from': accounts[0]}) == DEPOSIT
+    assert ren_pool.totalPooled() == DEPOSIT
