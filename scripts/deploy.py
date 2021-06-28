c74899df0a7eaee58846acd6653ff07c2f50a79c
fede-rodes <federodes@gmail.com>
2021-06-28 23:58:12 +0200

    replacing ERC20 implementation with openZeplin

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,5 @@
-from brownie import accounts, ERC20, RenPool
-# ^ accounts is coming from ganash and ERC20 and RenPool from the contracts folder.
+from brownie import accounts, RenToken, RenPool
+# ^ accounts is coming from Ganache while RenToken and RenPool from the contracts folder.
 
 def distribute_tokens(renToken):
   amount = 100_000 * 10 ** 18 # amount of tokens required to spin up a REN node
@@ -8,11 +8,11 @@ def distribute_tokens(renToken):
 
 def main():
   """
-  1. Mint ERC20 (REN) token. See decimals and totalSupply on https://etherscan.io/token/0x408e41876cccdc0f92210600ef50372656052a38#readContract;
+  1. Mint (ERC20) REN token;
   2. Distribute REN tokens among accounts;
   3. Deploy RenPool contract;
   """
-  renToken = ERC20.deploy("REN", "REN", 18, 1e28, {'from': accounts[0]})
+  renToken = RenToken.deploy({'from': accounts[0]})
   distribute_tokens(renToken)
   renPool = RenPool.deploy(renToken, {'from': accounts[0]})
   return renToken, renPool
