b9c726083200d98998fd98436ee087dce24a9a90
fede-rodes <federodes@gmail.com>
2021-07-16 18:21:46 +0200

    format input

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react'
 import { BigNumber } from '@ethersproject/bignumber'
+import { formatUnits, parseUnits } from '@ethersproject/units'
 import './App.css'
 import { useContract } from './hooks/useContract'
 import { NETWORKS, CONTRACT_NAMES, MAX_UINT256 } from './constants'
@@ -16,13 +17,13 @@ enum Actions {
 }
 
 export const App = (): JSX.Element => {
-  const { chainId, library, account } = useActiveWeb3React()
+  const { chainId, account } = useActiveWeb3React()
   const renToken = useContract(CONTRACT_NAMES.RenToken)
   const renPool = useContract(CONTRACT_NAMES.RenPool)
 
   const [totalPooled, setTotalPooled] = useState<BigNumber>(BigNumber.from(0))
   const [isApproved, setIsApproved] = useState(false)
-  const [input, setInput] = useState<string | null>()
+  const [input, setInput] = useState<string>('')
   const [disabled, setDisabled] = useState(false)
 
   // Query totalPooled once contracts are ready
@@ -38,7 +39,7 @@ export const App = (): JSX.Element => {
 
   const isTransferApproved = async (value: BigNumber): Promise<boolean> => {
     if (renToken == null) return false
-    if (value.lt(1)) return false
+    if (value.lt(BigNumber.from(1))) return false
     const allowance: BigNumber = await renToken.allowance(account, renPool.address)
     console.log('ALLOWANCE', allowance)
     return allowance.sub(value).gte(BigNumber.from(0))
@@ -49,9 +50,9 @@ export const App = (): JSX.Element => {
     const regex = /[0-9]/g
     const value = str.match(regex)?.join('') || ''
     setInput(value)
-    if (value == null) return
-    const isApproved = await isTransferApproved(BigNumber.from(value.padEnd(value.length + DECIMALS, '0')))
-    setIsApproved(isApproved)
+    if (value == null || value === '') return
+    const _isApproved = await isTransferApproved(BigNumber.from(parseUnits(value, DECIMALS)))
+    setIsApproved(_isApproved)
   }
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>, action: Actions): Promise<void> => {
@@ -69,9 +70,8 @@ export const App = (): JSX.Element => {
     if (action === Actions.approve) {
       const tx = await renToken.approve(renPool.address, MAX_UINT256)
       await tx.wait() // wait for mining
-      setIsApproved(await isTransferApproved(BigNumber.from(input.padEnd(input.length + DECIMALS, '0'))))
-      // .on('receipt', async () => { setIsApproved(await isTransferApproved(amount)) })
-      // .on('error', (e: any) => { console.log('Could not approve transfer', e) })
+      const _isApproved = await isTransferApproved(BigNumber.from(parseUnits(input, DECIMALS)))
+      setIsApproved(_isApproved)
     }
 
     if (action === Actions.deposit) {
@@ -82,16 +82,12 @@ export const App = (): JSX.Element => {
       }
 
       try {
-        const gasLimit = await library.estimateGas(renPool.deposit)
-        console.log('GAS LIMIT', gasLimit.toString())
-        const renAmount = BigNumber.from(input.padEnd(input.length + DECIMALS, '0')).toString()
+        const renAmount = BigNumber.from(parseUnits(input, DECIMALS))
         console.log('REN AMOUNT', renAmount)
-        const tx = await renPool.deposit(renAmount, { gasLimit: 172503 })
+        const tx = await renPool.deposit(renAmount, { gasLimit: 200000 })
         await tx.wait() // wait for mining
-        // await renPool.methods.deposit(renAmount).send({ from: account })
-        // .on('receipt', async () => {
-        const tp: string = await renPool.totalPooled({ gasLimit: 60000 })
-        setTotalPooled(BigNumber.from(tp))
+        const _totalPooled: BigNumber = await renPool.totalPooled({ gasLimit: 60000 })
+        setTotalPooled(BigNumber.from(_totalPooled))
         setInput('0')
       } catch (e) {
         alert(`Could not deposit, ${JSON.stringify(e, null, 2)}`)
@@ -105,20 +101,12 @@ export const App = (): JSX.Element => {
     try {
       const tx = await renToken.getFromFaucet({ gasLimit: 60000 })
       await tx.wait() // wait for mining
-      // await renToken.methods.getFromFaucet().send({ from: account })
     } catch (e) {
       alert(`Could not get from faucet ${JSON.stringify(e, null, 2)}`)
     }
   }
 
   const isAccountsUnlocked = account != null
-  console.log({ account })
-
-  const strTotalPooled = totalPooled.toString()
-  // TODO: use:
-  // ethers.utils.formatUnits(balance, 18)
-  // const dai = ethers.utils.parseUnits("1.0", 18)
-  // source: https://docs.ethers.io/v5/single-page/
 
   return (
     <>
@@ -126,34 +114,6 @@ export const App = (): JSX.Element => {
       <Wallet />
       <hr />
 
-      <div>
-        {/* {!!(library && account) && (
-          <button
-            onClick={() => {
-              library
-                .getSigner(account)
-                .signMessage('👋')
-                .then((signature: any) => {
-                  window.alert(`Success!\n\n${signature}`)
-                })
-                .catch((error: any) => {
-                  window.alert('Failure!' + (error && error.message ? `\n\n${error.message}` : ''))
-                })
-            }}
-          >
-            Sign Message
-          </button>
-        )} */}
-        {/* {!!(connector === connectorsByName[ConnectorNames.Network] && chainId) && (
-          <button
-            onClick={() => {
-              ;(connector as any).changeChainId(chainId === 1 ? CHAIN : 1)
-            }}
-          >
-            Switch Networks
-          </button>
-        )} */}
-      </div>
       <div className="App">
         {!isAccountsUnlocked && (
           <p><strong>Connect with Metamask and refresh the page to be able to edit the storage fields.</strong></p>
@@ -164,7 +124,7 @@ export const App = (): JSX.Element => {
         )}
 
         <h2>RenPool Contract</h2>
-        <div>The stored value is: {strTotalPooled.length > DECIMALS ? strTotalPooled.slice(0, strTotalPooled.length - DECIMALS) : strTotalPooled}</div>
+        <div>The stored value is: {formatUnits(totalPooled, DECIMALS)}</div>
         <br/>
         <form onSubmit={(e) => { handleSubmit(e, isApproved ? Actions.deposit : Actions.approve) }}>
           <div>
@@ -174,13 +134,13 @@ export const App = (): JSX.Element => {
               name="amount"
               type="text"
               value={input}
-              disabled={!isAccountsUnlocked}
+              disabled={!isAccountsUnlocked || disabled}
               onChange={handleChange}
             />
             <br/>
             <button
               type="submit"
-              disabled={!isAccountsUnlocked || disabled}
+              disabled={!isAccountsUnlocked || disabled || input == null || input.replaceAll('0', '') === ''}
             >
               {isApproved ? 'Deposit' : 'Approve'}
             </button>
