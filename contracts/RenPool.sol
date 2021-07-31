9b65b40c6e9d9c779169c14b4edbb6a1772b859a
fede-rodes <federodes@gmail.com>
2021-07-31 22:36:44 +0200

    starting with some changes

--- contracts/RenPool.sol
+++ contracts/RenPool.sol
@@ -1,19 +1,25 @@
 pragma solidity ^0.8.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol";
+
 contract RenPool {
+    ERC20 public renToken;
     address public owner; // This will be our address, in case we need to destroy the contract and refund everyone
     address public admin;
     mapping(address => uint) public balances;
     uint public totalPooled;
     bool public isLocked;
-    uint public fee;
+    uint public ownerFee; // Percentage
+    uint public adminFee; // Percentage
     uint public target;
+    uint8 public constant DECIMALS = 18;
+    uint public constant DEFAULT_TARGET = 100_000 * 10 ** uint(DECIMALS); // 100k ren for darknode
 
-    struct WithdrawRequest{
+    struct WithdrawRequest {
         address user;
         uint amount;
     }
-    
+
     WithdrawRequest[] public withdrawRequests;
 
     event RenDeposited(address from, uint amount); // Why add the time?
@@ -23,12 +29,17 @@ contract RenPool {
     event PoolUnlocked();
 
 
-    constructor() {
-        target = 100000; // 100k ren for darknode
-        owner = msg.sender;
+    constructor(address _renTokenAddr) {
+        renToken = ERC20(_renTokenAddr);
+        // ^ We'll need to implement to implement the ren token
+        // 0x408e41876cccdc0f92210600ef50372656052a38
+        target = DEFAULT_TARGET; // TODO: we need a set method to be able to update this value
+        owner = '0x0000000000000000000000000000000000000000'; // TODO
+        admin = msg.sender;
         isLocked = false;
         totalPooled = 0;
-        fee = 1;
+        ownerFee = 5;
+        adminFee = 5;
     }
 
 
@@ -47,44 +58,40 @@ contract RenPool {
         emit PoolLocked();
     }
 
-    function deposit() external payable{
-        // !! this is coded as if we were transfering ETH and not ren
-        // We'll need to implement to implement the ren token 
-        // 0x408e41876cccdc0f92210600ef50372656052a38
-        uint amount = msg.value;
+    function deposit(uint _amount) external {
+        address addr = msg.sender;
+
         require (amount > 0, "Invalid ammount amount.");
         require (amount + totalPooled < target, "Amount surpasses pool target");
+        require (isLocked == false, "Pool is locked");
 
+        renToken.transferFrom(addr, this, _amount);
+        // ^ user needs approve this transaction (give allowance) for the transferFrom method to pass.
+        // See: https://ethereum.org/nl/developers/tutorials/erc20-annotated-code/
+        balances[addr] += amount;
+        totalPooled += amount;
 
-        if(!isLocked){
-            // If pool is not locked, funds are still necesarry so no need to worry about the withdraw queue
+        emit RenDeposited(addr, amount);
 
-            balances[msg.sender] += amount;
-            totalPooled += amount;
-
-            emit RenDeposited(msg.sender, amount);
-
-            if(totalPooled == target){
-                _lockPool(); // Locking the pool is target is met
-            }
+        if(totalPooled == target){
+            _lockPool(); // Locking the pool if target is met
         }
-        else{
-            // If pool is locked, look if there is a withdraw queue
-            require(withdrawRequests.length > 0);
+    }
 
-            WithdrawRequest firstInLine = withdrawRequests[0];
-            // For now, the user who wants to get in a locked pool has to
-            // replace exactly the first in lines
-            require(firstInLine.amount == msg.amount);
-            balances[msg.sender] += amount;
-            balances[firstInLine.user] -= amount;
+    function requestDeposit(uint _amount) external {
+        require (amount > 0, "Invalid ammount amount.");
+        require (isLocked == true, "Pool is not locked");
+        require(withdrawRequests.length > 0);
 
-            // first in line withdraw funds
-            firstInLine.user.transfer(firstInLine.amount);
-        }
+        WithdrawRequest firstInLine = withdrawRequests[0];
+        // For now, the user who wants to get in a locked pool has to
+        // replace exactly the first in lines
+        require(firstInLine.amount == _amount);
+        balances[msg.sender] += _amount;
+        balances[firstInLine.user] -= _amount;
 
-
-
+        // First in line withdraw funds
+        firstInLine.user.transfer(firstInLine.amount);
     }
 
     function withdraw(uint _amount) external payable {
@@ -102,7 +109,7 @@ contract RenPool {
         }
         else{
             // Pool is locked, withdraw will be put in the queue
-            withdrawRequests.push(WithdrawRequest(user, _amount))
+            withdrawRequests.push(WithdrawRequest(user, _amount));
 
         }
 
