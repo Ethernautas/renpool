70860b115e724e12911db058870bf2e01c29e13b
Jonathan Stefanov <38321403+JonathanStefanov@users.noreply.github.com>
2021-07-31 12:30:32 +0200

    Withdraw logic

--- contracts/RenPool.sol
+++ contracts/RenPool.sol
@@ -9,11 +9,19 @@ contract RenPool {
     uint public fee;
     uint public target;
 
+    struct WithdrawRequest{
+        address user;
+        uint amount;
+    }
+    
+    WithdrawRequest[] public withdrawRequests;
+
     event RenDeposited(address from, uint amount); // Why add the time?
     event RenWithdraw(address from, uint amount);
     event PoolTargetReached();
     event PoolLocked();
     event PoolUnlocked();
+
 
     constructor() {
         target = 100000; // 100k ren for darknode
@@ -44,20 +52,73 @@ contract RenPool {
         // We'll need to implement to implement the ren token 
         // 0x408e41876cccdc0f92210600ef50372656052a38
         uint amount = msg.value;
-
         require (amount > 0, "Invalid ammount amount.");
         require (amount + totalPooled < target, "Amount surpasses pool target");
 
-        balances[msg.sender] += amount;
-        totalPooled += amount;
 
-        emit RenDeposited(msg.sender, amount);
+        if(!isLocked){
+            // If pool is not locked, funds are still necesarry so no need to worry about the withdraw queue
 
-        if(totalPooled == target){
-            _lockPool();
+            balances[msg.sender] += amount;
+            totalPooled += amount;
+
+            emit RenDeposited(msg.sender, amount);
+
+            if(totalPooled == target){
+                _lockPool(); // Locking the pool is target is met
+            }
         }
+        else{
+            // If pool is locked, look if there is a withdraw queue
+            require(withdrawRequests.length > 0);
+
+            WithdrawRequest firstInLine = withdrawRequests[0];
+            if(amount == firstInLine.amount){
+                // The amount is exactly what the requester wants
+                balances[msg.sender] += amount;
+                balances[firstInLine.user] _= amount;
+
+
+            }
+
+        }
+
+
 
     }
 
+    function withdraw(uint _amount) external payable {
+        require(balances[msg.sender] > 0 && balances[msg.sender] >=  _amount);
+        address payable user = payable(msg.sender);
+
+        if(!isLocked){
+            // The pool is not locked, user can withdraw right away
+            totalPooled -= _amount;
+            balances[msg.sender] += _amount;
+
+            // Again, we will transfer Ren no eth, will need to implement ren's ERC-20 smart contract later
+
+            user.transfer(_amount);
+        }
+        else{
+            // Pool is locked, withdraw will be put in the queue
+            withdrawRequests.push(WithdrawRequest(user, _amount))
+
+        }
+
+
+    }
+
+
+    // Function to remove element of array and changing the order accordingly
+    function _remove(WithdrawRequest index)  returns(WithdrawRequest[]) {
+        if (index >= array.length) return;
+
+        for (uint i = index; i<array.length-1; i++){
+            array[i] = array[i+1];
+        }
+        array.length--;
+        return array;
+    }
 
 }
