5318a8029381e6e3bae4b8fa8bb49c977ecee823
Jonathan Stefanov <38321403+JonathanStefanov@users.noreply.github.com>
2021-08-04 18:57:18 +0200

    Fixed conflicts

--- tests/test_ren_pool.py
+++ tests/test_ren_pool.py
@@ -55,3 +55,27 @@ def test_ren_pool_withdraw(ren_pool, ren_token, user):
     assert ren_pool.balanceOf(user) == 0
     assert ren_pool.totalPooled() == 0
     assert ren_token.balanceOf(ren_pool) == 0
+
+def test_withdraw_fullfulment(owner, user, ren_pool, ren_token):
+    # Depositing 100k in the pool to lock it
+    assert ren_pool.totalPooled() == 0
+        
+    ren_token.approve(ren_pool.address, C.POOL_TARGET, {'from': owner})
+    ren_pool.deposit(C.POOL_TARGET, {'from': owner})
+    
+    ren_pool.withdraw(C.WITHDRAW_AMOUNT, {'from': owner}) # The pool is locked so this will create a withdraw request
+
+    assert ren_pool.totalPooled() == C.POOL_TARGET
+    assert ren_pool.withdrawRequests(0) # seeing if there is a withdraw request
+    
+    # Fullfilling
+    ren_token.callFaucet({'from': user})
+    assert ren_token.balanceOf(user, {'from': user}) == C.WITHDRAW_AMOUNT # Account 1 has 1k REN
+    
+    # Account 1 fullfills the withdraw request
+    ren_token.approve(ren_pool.address, C.WITHDRAW_AMOUNT , {'from': user})
+    ren_pool.fullfillWithdrawRequest(0, {'from': user}) # Filfilling the request
+    
+    assert ren_token.balanceOf(user, {'from': user}) == 0 # Account has fullfiled the withdraw request
+    assert ren_pool.balanceOf(user, {'from': user}) == C.WITHDRAW_AMOUNT
+    assert ren_pool.totalPooled() == C.POOL_TARGET # Pool still full
