2e42fb00c373927ef67cee7819c4e447b2b82d74
fede-rodes <federodes@gmail.com>
2021-09-04 22:55:20 +0200

    cleaning form

--- client/src/components/DarknodeUrlForm/index.tsx
+++ client/src/components/DarknodeUrlForm/index.tsx
@@ -1,13 +1,5 @@
-import React, { FC, useContext, useState, useEffect, ChangeEvent, FormEvent } from 'react'
-import { BigNumber } from '@ethersproject/bignumber'
-import { formatBytes32String } from '@ethersproject/strings'
+import React, { FC, useState, ChangeEvent, FormEvent } from 'react'
 import { Box, Form, Button } from 'rimble-ui'
-import { BOND } from '../../constants'
-import { darknodeIDBase58ToHex } from '../../utils/base58ToHex'
-import { DarknodeRegistryContext } from '../../context/DarknodeRegistryProvider'
-import { RenTokenContext } from '../../context/RenTokenProvider'
-import { RenPoolContext } from '../../context/RenPoolProvider'
-import { useActiveWeb3React } from '../../hooks/useActiveWeb3React'
 import { validDarknodeUrl, getDarknodeUrlParams } from '../../utils/darknodeUrl'
 
 const CHAIN_ID = process.env.REACT_APP_CHAIN_ID
@@ -16,20 +8,13 @@ const DARKNODE_NETWORK = CHAIN_ID === '1' ? 'mainnet' : 'testnet'
 const DARKNODE_BASE_URL = `https://${DARKNODE_NETWORK}.renproject.io/darknode/`
 const DARKNODE_URL_PLACEHOLDER = `https://${DARKNODE_NETWORK}.renproject.io/darknode/<YOUR-DARKNODE-ID>?action=register&public_key=<YOUR-PUBLIC-KEY>&name=<YOUR-DARKNODE-NAME>`
 
-enum Actions {
-  APPROVE = 'APPROVE',
-  REGISTER = 'REGISTER',
-}
-
 export interface DarknodeUrlFormProps {
   btnLabel: string
   disabled: boolean
   onBeforeHook: () => void
   onClientCancelHook: () => void
   onClientErrorHook: (err?: string) => void
-  onSuccessHook: ({ darknodeID: string, publicKey: string, action: Actions }) => void
-  // onApprove: () => void
-  // onRegister: () => void
+  onSuccessHook: ({ darknodeID, publicKey }: { darknodeID: string, publicKey: string }) => void
 }
 
 export const DarknoneUrlForm: FC<DarknodeUrlFormProps> = ({
@@ -39,43 +24,14 @@ export const DarknoneUrlForm: FC<DarknodeUrlFormProps> = ({
   onClientCancelHook,
   onClientErrorHook,
   onSuccessHook,
-  // onApprove,
-  // onRegister,
 }): JSX.Element => {
-  // const { account } = useActiveWeb3React()
-  // const { darknodeRegistry } = useContext(DarknodeRegistryContext)
-  // const { renToken } = useContext(RenTokenContext)
-  // const { renPool, isLocked } = useContext(RenPoolContext)
-
-  // const [isApproved, setIsApproved] = useState(false)
   const [darknodeUrl, setDarknodeUrl] = useState<string>('')
-
-  // useEffect(() => {
-  //   if (renToken != null && account != null) {
-  //     checkForApproval(BOND)
-  //       .then((_isApproved: boolean) => { setIsApproved(_isApproved) })
-  //       .catch((e: Error) => { alert(`Error checking for approval ${JSON.stringify(e, null, 2)}`) })
-  //   }
-  // }, [renToken])
-
-  // const checkForApproval = async (value: BigNumber): Promise<boolean> => {
-  //   if (renToken == null || darknodeRegistry == null) return false
-  //   if (value.lt(BigNumber.from(1))) return false
-  //   const allowance: BigNumber = await renToken.allowance(renPool.address, darknodeRegistry.address)
-  //   return allowance.sub(value).gte(BigNumber.from(0))
-  // }
-
-  // TODO: after node registration, query status from DarknodeRegistry
-
-
-
-
 
   const handleChange = async (e: ChangeEvent<HTMLTextAreaElement>): Promise<void> => {
     setDarknodeUrl(e.target.value || '')
   }
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>, action: Actions): Promise<void> => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault()
 
     // Run before hook logic if provided and return on error
@@ -86,7 +42,7 @@ export const DarknoneUrlForm: FC<DarknodeUrlFormProps> = ({
       return // return silently
     }
 
-    // Validate fields
+    // Validate input
     const { isValid, err } = validDarknodeUrl(darknodeUrl, DARKNODE_BASE_URL)
 
     // In case of errors, display on UI and return handler to parent component
@@ -100,40 +56,15 @@ export const DarknoneUrlForm: FC<DarknodeUrlFormProps> = ({
     const { darknodeID, publicKey } = getDarknodeUrlParams(darknodeUrl, DARKNODE_BASE_URL)
 
     // Pass event up to parent component
-    onSuccessHook({ darknodeID, publicKey, action })
-
-    // if (action === Actions.APPROVE) {
-    //   const tx = await renPool.approveBondTransfer({ gasLimit: 200000 })
-    //   await tx.wait() // wait for mining
-    //   const _isApproved = await checkForApproval(BOND)
-    //   setIsApproved(_isApproved)
-    // }
-
-    // try {
-    //   const { darknodeID, publicKey } = getDarknodeUrlParams(darknodeUrl, DARKNODE_BASE_URL)
-    //   console.log('DARKNODE_ID', darknodeID, 'PUBLIC_KEY', publicKey)
-    //   const tx = await renPool.registerDarknode(darknodeIDBase58ToHex(darknodeID), formatBytes32String(publicKey), { gasLimit: 20000000 })
-    //   await tx.wait() // wait for mining
-    //   setDarknodeUrl('')
-    // } catch (e) {
-    //   alert(`Could not register, ${JSON.stringify(e, null, 2)}`)
-    // }
-
-    // setDisabled(false)
+    onSuccessHook({ darknodeID, publicKey })
   }
 
-  const _disabled = !isLocked
-
   return (
-    <Form
-      onSubmit={(e: FormEvent<HTMLFormElement>) => {
-        handleSubmit(e, isApproved ? Actions.REGISTER : Actions.APPROVE)
-      }}
-    >
+    <Form onSubmit={handleSubmit}>
       <textarea
         value={darknodeUrl}
         placeholder={DARKNODE_URL_PLACEHOLDER}
-        disabled={disabled || _disabled}
+        disabled={disabled}
         rows={13}
         style={{ width: '100%' }}
         onChange={handleChange}
@@ -141,11 +72,10 @@ export const DarknoneUrlForm: FC<DarknodeUrlFormProps> = ({
       <Box p={2} />
       <Button
         type="submit"
-        variant={isApproved ? 'success' : ''}
-        disabled={disabled || _disabled}
+        variant="success"
+        disabled={disabled}
         width={1}
       >
-        {/* {isApproved ? 'Register darknode' : 'Approve registration'} */}
         {btnLabel}
       </Button>
     </Form>
