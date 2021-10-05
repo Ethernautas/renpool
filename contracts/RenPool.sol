f52ac38bd139d06bd12c2b486774cacaf81eb970
fede-rodes <federodes@gmail.com>
2021-10-05 11:58:20 +0200

    format

--- contracts/RenPool.sol
+++ contracts/RenPool.sol
@@ -9,281 +9,281 @@ import "../interfaces/IDarknodeRegistry.sol";
 // TODO: use safeMath?
 
 contract RenPool {
-    uint8 public constant DECIMALS = 18;
+	uint8 public constant DECIMALS = 18;
 
-    address public renTokenAddr;
-    address public darknodeRegistryAddr;
-    address public owner; // This will be our address, in case we need to destroy the contract and refund everyone
-    address public nodeOperator;
-    address public darknodeID;
+	address public renTokenAddr;
+	address public darknodeRegistryAddr;
+	address public owner; // This will be our address, in case we need to destroy the contract and refund everyone
+	address public nodeOperator;
+	address public darknodeID;
 
-    bytes public publicKey;
-    // ^ What happens if we register and deregister and register back again?
+	bytes public publicKey;
+	// ^ What happens if we register and deregister and register back again?
 
-    uint public bond;
-    uint public totalPooled;
-    uint public ownerFee; // Percentage
-    uint public nodeOperatorFee; // Percentage
+	uint public bond;
+	uint public totalPooled;
+	uint public ownerFee; // Percentage
+	uint public nodeOperatorFee; // Percentage
 
-    bool public isLocked;
+	bool public isLocked;
 
-    mapping(address => uint) public balances;
-    mapping(address => uint) public withdrawRequests;
+	mapping(address => uint) public balances;
+	mapping(address => uint) public withdrawRequests;
 
-    IERC20 public renToken;
-    IDarknodeRegistry public darknodeRegistry;
+	IERC20 public renToken;
+	IDarknodeRegistry public darknodeRegistry;
 
-    event RenDeposited(address indexed _from, uint _amount);
-    event RenWithdrawn(address indexed _from, uint _amount);
-    event EthDeposited(address indexed _from, uint _amount);
-    event EthWithdrawn(address indexed _from, uint _amount);
-    event PoolLocked();
-    event PoolUnlocked();
+	event RenDeposited(address indexed _from, uint _amount);
+	event RenWithdrawn(address indexed _from, uint _amount);
+	event EthDeposited(address indexed _from, uint _amount);
+	event EthWithdrawn(address indexed _from, uint _amount);
+	event PoolLocked();
+	event PoolUnlocked();
 
-    /**
-     * TODO
-     */
-    constructor(
-        address _renTokenAddr,
-        address _darknodeRegistryAddr,
-        address _owner,
-        uint _bond
-    )
-    {
-        renTokenAddr = _renTokenAddr;
-        darknodeRegistryAddr = _darknodeRegistryAddr;
-        owner = _owner;
-        nodeOperator = msg.sender;
-        renToken = IERC20(_renTokenAddr);
-        darknodeRegistry = IDarknodeRegistry(_darknodeRegistryAddr);
-        bond = _bond;
-        isLocked = false;
-        totalPooled = 0;
-        ownerFee = 5;
-        nodeOperatorFee = 5;
+	/**
+	 * TODO
+	 */
+	constructor(
+		address _renTokenAddr,
+		address _darknodeRegistryAddr,
+		address _owner,
+		uint _bond
+	)
+	{
+		renTokenAddr = _renTokenAddr;
+		darknodeRegistryAddr = _darknodeRegistryAddr;
+		owner = _owner;
+		nodeOperator = msg.sender;
+		renToken = IERC20(_renTokenAddr);
+		darknodeRegistry = IDarknodeRegistry(_darknodeRegistryAddr);
+		bond = _bond;
+		isLocked = false;
+		totalPooled = 0;
+		ownerFee = 5;
+		nodeOperatorFee = 5;
 
-        // TODO: register pool into RenPoolStore
-    }
+		// TODO: register pool into RenPoolStore
+	}
 
-    modifier onlyNodeOperator() {
-        require (
-            msg.sender == nodeOperator,
-            "Caller is not nodeOperator"
-        );
-        _;
-    }
+	modifier onlyNodeOperator() {
+		require (
+			msg.sender == nodeOperator,
+			"Caller is not nodeOperator"
+		);
+		_;
+	}
 
-    modifier onlyOwnerNodeOperator() {
-        require (
-            msg.sender == owner || msg.sender == nodeOperator,
-            "Caller is not owner/nodeOperator"
-        );
-        _;
-    }
+	modifier onlyOwnerNodeOperator() {
+		require (
+			msg.sender == owner || msg.sender == nodeOperator,
+			"Caller is not owner/nodeOperator"
+		);
+		_;
+	}
 
-    modifier onlyOwner() {
-        require (
-            msg.sender == owner,
-            "Caller is not owner"
-        );
-        _;
-    }
+	modifier onlyOwner() {
+		require (
+			msg.sender == owner,
+			"Caller is not owner"
+		);
+		_;
+	}
 
-    /**
-     * @notice Lock pool so that no direct deposits/withdrawals can
-     * be performed.
-     */
-    function _lockPool() private {
-        isLocked = true;
-        emit PoolLocked();
-    }
+	/**
+	 * @notice Lock pool so that no direct deposits/withdrawals can
+	 * be performed.
+	 */
+	function _lockPool() private {
+		isLocked = true;
+		emit PoolLocked();
+	}
 
-    function unlockPool() external onlyOwnerNodeOperator {
-        require(renToken.balanceOf(address(this)) > 0, "Pool balance is zero");
-        isLocked = false;
-        emit PoolUnlocked();
-    }
+	function unlockPool() external onlyOwnerNodeOperator {
+		require(renToken.balanceOf(address(this)) > 0, "Pool balance is zero");
+		isLocked = false;
+		emit PoolUnlocked();
+	}
 
-    /**
-     * @notice Deposit REN into the RenPool contract. Before depositing,
-     * the transfer must be approved in the REN contract. In case the
-     * predefined bond is reached, the pool is locked preventing any
-     * further deposits or withdrawals.
-     *
-     * @param _amount The amount of REN to be deposited into the pool.
-     */
-    function deposit(uint _amount) external {
-        address sender = msg.sender;
+	/**
+	 * @notice Deposit REN into the RenPool contract. Before depositing,
+	 * the transfer must be approved in the REN contract. In case the
+	 * predefined bond is reached, the pool is locked preventing any
+	 * further deposits or withdrawals.
+	 *
+	 * @param _amount The amount of REN to be deposited into the pool.
+	 */
+	function deposit(uint _amount) external {
+		address sender = msg.sender;
 
-        require(isLocked == false, "Pool is locked");
-        require(_amount > 0, "Invalid amount");
-        require(_amount + totalPooled <= bond, "Amount surpasses bond");
+		require(isLocked == false, "Pool is locked");
+		require(_amount > 0, "Invalid amount");
+		require(_amount + totalPooled <= bond, "Amount surpasses bond");
 
-        balances[sender] += _amount;
-        totalPooled += _amount;
+		balances[sender] += _amount;
+		totalPooled += _amount;
 
-        require(
-            renToken.transferFrom(sender, address(this), _amount) == true,
-            "Deposit failed"
-        );
+		require(
+			renToken.transferFrom(sender, address(this), _amount) == true,
+			"Deposit failed"
+		);
 
-        emit RenDeposited(sender, _amount);
+		emit RenDeposited(sender, _amount);
 
-        if (totalPooled == bond) {
-            _lockPool();
-        }
-    }
+		if (totalPooled == bond) {
+			_lockPool();
+		}
+	}
 
-    /**
-     * @notice Withdraw REN tokens while the pool is still open.
-     */
-    function withdraw(uint _amount) external {
-        address sender = msg.sender;
-        uint senderBalance = balances[sender];
+	/**
+	 * @notice Withdraw REN tokens while the pool is still open.
+	 */
+	function withdraw(uint _amount) external {
+		address sender = msg.sender;
+		uint senderBalance = balances[sender];
 
-        require(senderBalance > 0 && senderBalance >= _amount, "Insufficient funds");
-        require(isLocked == false, "Pool is locked");
+		require(senderBalance > 0 && senderBalance >= _amount, "Insufficient funds");
+		require(isLocked == false, "Pool is locked");
 
-        totalPooled -= _amount;
-        balances[sender] -= _amount;
+		totalPooled -= _amount;
+		balances[sender] -= _amount;
 
-        require(
-            renToken.transfer(sender, _amount) == true,
-            "Withdraw failed"
-        );
+		require(
+			renToken.transfer(sender, _amount) == true,
+			"Withdraw failed"
+		);
 
-        emit RenWithdrawn(sender, _amount);
-    }
+		emit RenWithdrawn(sender, _amount);
+	}
 
-    /**
-     * TODO
-     * @dev Users can have up to a single request active. In case of several
-     * calls to this method, only the last request will be preserved.
-     */
-    function requestWithdraw(uint _amount) external {
-        address sender = msg.sender;
-        uint senderBalance = balances[sender];
+	/**
+	 * TODO
+	 * @dev Users can have up to a single request active. In case of several
+	 * calls to this method, only the last request will be preserved.
+	 */
+	function requestWithdraw(uint _amount) external {
+		address sender = msg.sender;
+		uint senderBalance = balances[sender];
 
-        require(senderBalance > 0 && senderBalance >= _amount, "Insufficient funds");
-        require(isLocked == true, "Pool is not locked");
+		require(senderBalance > 0 && senderBalance >= _amount, "Insufficient funds");
+		require(isLocked == true, "Pool is not locked");
 
-        withdrawRequests[sender] = _amount;
+		withdrawRequests[sender] = _amount;
 
-        // TODO emit event
-    }
+		// TODO emit event
+	}
 
-    /**
-     * TODO
-     */
-    function fulfillWithdrawRequest(address _target) external {
-        address sender = msg.sender;
-        uint amount = withdrawRequests[_target];
-        // ^ This could not be defined plus make sure amount > 0
-        // TODO: make sure user cannot fullfil his own request
-        // TODO: add test for when _target doesn't have an associated withdrawRequest
+	/**
+	 * TODO
+	 */
+	function fulfillWithdrawRequest(address _target) external {
+		address sender = msg.sender;
+		uint amount = withdrawRequests[_target];
+		// ^ This could not be defined plus make sure amount > 0
+		// TODO: make sure user cannot fullfil his own request
+		// TODO: add test for when _target doesn't have an associated withdrawRequest
 
-        require(isLocked == true, "Pool is not locked");
+		require(isLocked == true, "Pool is not locked");
 
-        balances[sender] += amount;
-        balances[_target] -= amount;
-        delete withdrawRequests[_target];
+		balances[sender] += amount;
+		balances[_target] -= amount;
+		delete withdrawRequests[_target];
 
-        // Transfer funds from sender to _target
-        require(
-            renToken.transferFrom(sender, address(this), amount) == true,
-            "Deposit failed"
-        );
-        require(
-            renToken.transfer(_target, amount) == true,
-            "Refund failed"
-        );
+		// Transfer funds from sender to _target
+		require(
+			renToken.transferFrom(sender, address(this), amount) == true,
+			"Deposit failed"
+		);
+		require(
+			renToken.transfer(_target, amount) == true,
+			"Refund failed"
+		);
 
-        // TODO emit event
-    }
+		// TODO emit event
+	}
 
-    // TODO: cancelWithdrawRequest
-    // TODO: getWithdrawRequests
+	// TODO: cancelWithdrawRequest
+	// TODO: getWithdrawRequests
 
-    /**
-     * @notice Return REN balance for the given address.
-     *
-     * @param _target Address to be queried.
-     */
-    function balanceOf(address _target) external view returns(uint) {
-        return balances[_target];
-    }
+	/**
+	 * @notice Return REN balance for the given address.
+	 *
+	 * @param _target Address to be queried.
+	 */
+	function balanceOf(address _target) external view returns(uint) {
+		return balances[_target];
+	}
 
-    /**
-     * @notice Transfer bond to the darknodeRegistry contract prior to
-     * registering the darknode.
-     */
-    function approveBondTransfer() external onlyNodeOperator {
-        require(isLocked == true, "Pool is not locked");
+	/**
+	 * @notice Transfer bond to the darknodeRegistry contract prior to
+	 * registering the darknode.
+	 */
+	function approveBondTransfer() external onlyNodeOperator {
+		require(isLocked == true, "Pool is not locked");
 
-        require(
-            renToken.approve(darknodeRegistryAddr, bond) == true,
-            "Bond transfer failed"
-        );
-    }
+		require(
+			renToken.approve(darknodeRegistryAddr, bond) == true,
+			"Bond transfer failed"
+		);
+	}
 
-    /**
-     * @notice Register a darknode and transfer the bond to the darknodeRegistry
-     * contract. Before registering, the bond transfer must be approved in the
-     * darknodeRegistry contract (see approveTransferBond). The caller must
-     * provide a public encryption key for the darknode. The darknode will remain
-     * pending registration until the next epoch. Only after this period can the
-     * darknode be deregistered. The caller of this method will be stored as the
-     * owner of the darknode.
-     *
-     * @param _darknodeID The darknode ID that will be registered.
-     * @param _publicKey The public key of the darknode. It is stored to allow
-     * other darknodes and traders to encrypt messages to the trader.
-     */
-    function registerDarknode(address _darknodeID, bytes calldata _publicKey) external onlyNodeOperator {
-        require(isLocked == true, "Pool is not locked");
+	/**
+	 * @notice Register a darknode and transfer the bond to the darknodeRegistry
+	 * contract. Before registering, the bond transfer must be approved in the
+	 * darknodeRegistry contract (see approveTransferBond). The caller must
+	 * provide a public encryption key for the darknode. The darknode will remain
+	 * pending registration until the next epoch. Only after this period can the
+	 * darknode be deregistered. The caller of this method will be stored as the
+	 * owner of the darknode.
+	 *
+	 * @param _darknodeID The darknode ID that will be registered.
+	 * @param _publicKey The public key of the darknode. It is stored to allow
+	 * other darknodes and traders to encrypt messages to the trader.
+	 */
+	function registerDarknode(address _darknodeID, bytes calldata _publicKey) external onlyNodeOperator {
+		require(isLocked == true, "Pool is not locked");
 
-        darknodeRegistry.register(_darknodeID, _publicKey);
+		darknodeRegistry.register(_darknodeID, _publicKey);
 
-        darknodeID = _darknodeID;
-        publicKey = _publicKey;
-    }
+		darknodeID = _darknodeID;
+		publicKey = _publicKey;
+	}
 
-    /**
-     * @notice Deregister a darknode. The darknode will not be deregistered
-     * until the end of the epoch. After another epoch, the bond can be
-     * refunded by calling the refund method.
-     *
-     * @dev We don't reset darknodeID/publicKey values after deregistration in order
-     * to being able to call refund.
-     */
-    function deregisterDarknode() external onlyOwnerNodeOperator {
-        darknodeRegistry.deregister(darknodeID);
-    }
+	/**
+	 * @notice Deregister a darknode. The darknode will not be deregistered
+	 * until the end of the epoch. After another epoch, the bond can be
+	 * refunded by calling the refund method.
+	 *
+	 * @dev We don't reset darknodeID/publicKey values after deregistration in order
+	 * to being able to call refund.
+	 */
+	function deregisterDarknode() external onlyOwnerNodeOperator {
+		darknodeRegistry.deregister(darknodeID);
+	}
 
-    /**
-     * @notice Refund the bond of a deregistered darknode. This will make the
-     * darknode available for registration again. Anyone can call this function
-     * but the bond will always be refunded to the darknode owner.
-     *
-     * @dev No need to reset darknodeID/publicKey values after refund.
-     */
-    function refundBond() external {
-        darknodeRegistry.refund(darknodeID);
-    }
+	/**
+	 * @notice Refund the bond of a deregistered darknode. This will make the
+	 * darknode available for registration again. Anyone can call this function
+	 * but the bond will always be refunded to the darknode owner.
+	 *
+	 * @dev No need to reset darknodeID/publicKey values after refund.
+	 */
+	function refundBond() external {
+		darknodeRegistry.refund(darknodeID);
+	}
 
-    /**
-     * @notice Allow ETH deposits in case gas is necessary to pay for transactions.
-     */
-    receive() external payable {
-        emit EthDeposited(msg.sender, msg.value);
-    }
+	/**
+	 * @notice Allow ETH deposits in case gas is necessary to pay for transactions.
+	 */
+	receive() external payable {
+		emit EthDeposited(msg.sender, msg.value);
+	}
 
-    /**
-     * @notice Allow node operator to withdraw any remaining gas.
-     */
-    function withdrawGas() external onlyNodeOperator {
-        uint balance = address(this).balance;
-        payable(nodeOperator).transfer(balance);
-        emit EthWithdrawn(nodeOperator, balance);
-    }
+	/**
+	 * @notice Allow node operator to withdraw any remaining gas.
+	 */
+	function withdrawGas() external onlyNodeOperator {
+		uint balance = address(this).balance;
+		payable(nodeOperator).transfer(balance);
+		emit EthWithdrawn(nodeOperator, balance);
+	}
 }
--- interfaces/IDarknodeRegistry.sol
+++ interfaces/IDarknodeRegistry.sol
@@ -1,76 +1,76 @@
 pragma solidity ^0.8.0;
 
 interface IDarknodeRegistry {
-    /**
-     * @notice Register a darknode and transfer the bond to the REN contract.
-     * Before registering, the bond transfer must be approved in the REN
-     * contract. The caller must provide a public encryption key for the
-     * darknode. The darknode will remain pending registration until the next
-     * epoch. Only after this period can the darknode be deregistered. The
-     * caller of this method will be stored as the owner of the darknode.
-     *
-     * @param _darknodeID The darknode ID that will be registered.
-     * @param _publicKey The public key of the darknode. It is stored to allow
-     * other darknodes and traders to encrypt messages to the trader.
-     */
-    function register(address _darknodeID, bytes calldata _publicKey) external;
+	/**
+		* @notice Register a darknode and transfer the bond to the REN contract.
+		* Before registering, the bond transfer must be approved in the REN
+		* contract. The caller must provide a public encryption key for the
+		* darknode. The darknode will remain pending registration until the next
+		* epoch. Only after this period can the darknode be deregistered. The
+		* caller of this method will be stored as the owner of the darknode.
+		*
+		* @param _darknodeID The darknode ID that will be registered.
+		* @param _publicKey The public key of the darknode. It is stored to allow
+		* other darknodes and traders to encrypt messages to the trader.
+		*/
+	function register(address _darknodeID, bytes calldata _publicKey) external;
 
-    /**
-     * @notice Retrieves the address of the account that registered a darknode.
-     *
-     * @param _darknodeID The ID of the darknode to retrieve the owner for.
-     */
-    function getDarknodeOperator(address _darknodeID) external view returns (address payable);
+	/**
+		* @notice Retrieves the address of the account that registered a darknode.
+		*
+		* @param _darknodeID The ID of the darknode to retrieve the owner for.
+		*/
+	function getDarknodeOperator(address _darknodeID) external view returns (address payable);
 
-    /**
-     * @notice Deregister a darknode. The darknode will not be deregistered
-     * until the end of the epoch. After another epoch, the bond can be
-     * refunded by calling the refund method.
-     *
-     * @param _darknodeID The darknode ID that will be deregistered. The caller
-     * of this method store.darknodeRegisteredAt(_darknodeID) must be
-     * the owner of this darknode.
-     */
-    function deregister(address _darknodeID) external;
+	/**
+		* @notice Deregister a darknode. The darknode will not be deregistered
+		* until the end of the epoch. After another epoch, the bond can be
+		* refunded by calling the refund method.
+		*
+		* @param _darknodeID The darknode ID that will be deregistered. The caller
+		* of this method store.darknodeRegisteredAt(_darknodeID) must be
+		* the owner of this darknode.
+		*/
+	function deregister(address _darknodeID) external;
 
-    /**
-     * @notice Refund the bond of a deregistered darknode. This will make the
-     * darknode available for registration again. Anyone can call this function
-     * but the bond will always be refunded to the darknode owner.
-     *
-     * @param _darknodeID The darknode ID that will be refunded. The caller
-     * of this method must be the owner of this darknode.
-     */
-    function refund(address _darknodeID) external;
+	/**
+		* @notice Refund the bond of a deregistered darknode. This will make the
+		* darknode available for registration again. Anyone can call this function
+		* but the bond will always be refunded to the darknode owner.
+		*
+		* @param _darknodeID The darknode ID that will be refunded. The caller
+		* of this method must be the owner of this darknode.
+		*/
+	function refund(address _darknodeID) external;
 
-    /**
-     * @notice Returns whether a darknode is scheduled to become registered
-     * at next epoch.
-     *
-     * @param _darknodeID The ID of the darknode to return
-     */
-    function isPendingRegistration(address _darknodeID) external view returns (bool);
+	/**
+		* @notice Returns whether a darknode is scheduled to become registered
+		* at next epoch.
+		*
+		* @param _darknodeID The ID of the darknode to return
+		*/
+	function isPendingRegistration(address _darknodeID) external view returns (bool);
 
-    /**
-     * @notice Returns if a darknode is in the pending deregistered state. In
-     * this state a darknode is still considered registered.
-     */
-    function isPendingDeregistration(address _darknodeID) external view returns (bool);
+	/**
+		* @notice Returns if a darknode is in the pending deregistered state. In
+		* this state a darknode is still considered registered.
+		*/
+	function isPendingDeregistration(address _darknodeID) external view returns (bool);
 
-    /**
-     * @notice Returns if a darknode is in the registered state.
-     */
-    function isRegistered(address _darknodeID) external view returns (bool);
+	/**
+		* @notice Returns if a darknode is in the registered state.
+		*/
+	function isRegistered(address _darknodeID) external view returns (bool);
 
-    /**
-     * @notice Returns if a darknode is in the deregistered state.
-     */
-    function isDeregistered(address _darknodeID) external view returns (bool);
+	/**
+		* @notice Returns if a darknode is in the deregistered state.
+		*/
+	function isDeregistered(address _darknodeID) external view returns (bool);
 
-    /**
-     * @notice Progress the epoch if it is possible to do so. This captures
-     * the current timestamp and current blockhash and overrides the current
-     * epoch.
-     */
-    function epoch() external;
+	/**
+		* @notice Progress the epoch if it is possible to do so. This captures
+		* the current timestamp and current blockhash and overrides the current
+		* epoch.
+		*/
+	function epoch() external;
 }
