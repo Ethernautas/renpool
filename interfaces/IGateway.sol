a76a5015f1b0865a6b7e87174bf7b6c39603cfdd
fede-rodes <federodes@gmail.com>
2021-10-05 14:44:19 +0200

    format

--- interfaces/IGateway.sol
+++ interfaces/IGateway.sol
@@ -12,17 +12,18 @@ interface IMintGateway {
 }
 
 interface IBurnGateway {
-    function burn(bytes calldata _to, uint256 _amountScaled) external returns (uint256);
-    function burnFee() external view returns (uint256);
+	function burn(bytes calldata _to, uint256 _amountScaled) external returns (uint256);
+	function burnFee() external view returns (uint256);
 }
 
 // TODO: In ^0.6.0, should be `interface IGateway is IMintGateway,IBurnGateway {}`
-interface IGateway { // is IMintGateway
+interface IGateway {
 	/**
 	 * @param _amount The amount of Bitcoin provided to the Darknodes in Sats.
 	 * @param _nHash The hash of the nonce returned by the Darknodes.
 	 * @param _sig The signature returned by the Darknodes.
 	 */
+	// is IMintGateway
 	function mint(
 		bytes32 _pHash,
 		uint256 _amount,
