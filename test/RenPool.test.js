c95e688841b305f4263e566dd15b65149462bd66
Luis Mastrangelo <Luis_Mastrangelo@epam.com>
2021-11-15 15:49:00 +0100

    Increase blockchain time for reward test

--- test/RenPool.test.js
+++ test/RenPool.test.js
@@ -233,6 +233,8 @@ describe('RenPool contract test', function () {
 
     const NODE_ID = base58ToHex('8MHJ9prQt7UGupfZKSMVes3VzPrGBB');
     const PUBLIC_KEY = '0x000000077373682d727361000000030100010000010100d0feba4ae65ea9ad771d153419bcc21189d954b6bf75fd5488055cd2641231014f190c0e059a452d301c535e931df33590ec0e18c59341a2766cc885d1dc6e66f5cc65b94522ec944ae4200bd56a30223328b258d50b507dd94b4c4742768f3fec2b815c9c4b0fe26727e82865f6a064fa3ff2443d135d9788095a1c17487fd5c389a491c16b73385d516a303debc3bcccae337a7ec0d89d51ce05262a0c4c1f2178466c85379b8cd4e5cbe1c90a05fb0c1ed3eee2134774b450e7b0b70c792abad55beef919e21a03cb9de4e963a820c2f84421a4559d0b67cfd17c1686ff6f2d1bb07ac2c82cede1cf5f16a57e125a29fef65891715b061606bca1a0eb026b';
+    
+    const ONE_MONTH = 60 * 60 * 24 * 31;
 
     it('should transfer reward to darknode owner', async function () {
       await renToken.connect(bob).approve(renPool.address, POOL_BOND);
@@ -241,12 +243,12 @@ describe('RenPool contract test', function () {
       await renPool.connect(nodeOperator).approveBondTransfer();
       await renPool.connect(nodeOperator).registerDarknode(NODE_ID, PUBLIC_KEY);
 
-      // TODO: set up chain.mine(timedelta=C.ONE_MONTH)
+      await provider.send('evm_increaseTime', [ONE_MONTH]);
       await darknodeRegistry.connect(alice).epoch();
 
       expect(await darknodeRegistry.connect(alice).isRegistered(NODE_ID)).to.be.true;
 
-      // TODO: set up chain.mine(timedelta=C.ONE_MONTH)
+      await provider.send('evm_increaseTime', [ONE_MONTH]);
       await darknodeRegistry.connect(alice).epoch();
 
       await renPool.transferRewardsToDarknodeOwner([renBTC]);
