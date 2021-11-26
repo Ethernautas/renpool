be0f2bea4c88ebf02b22c8a17ba4ef075c95bce7
Luis Mastrangelo <luismastrangelo@gmail.com>
2021-11-26 16:17:54 +0100

    Add explanation where those values came from

--- test/ren/renvm.check.js
+++ test/ren/renvm.check.js
@@ -2,6 +2,45 @@ const { expect } = require('chai');
 const { generateTransactionHash, PackPrimitive, toURLBase64 } = require('@renproject/utils');
 
 describe("generateTransactionHash", () => {
+
+    /**
+     * These values were obtained using the following JSON-RPC query and
+     * choosing one random transaction using the `BTC/toEthereum` selector.
+     * 
+     * ```
+     * POST https://lightnode-testnet.herokuapp.com HTTP/1.1
+     * Content-Type: application/json
+     * Accept: application/json
+     *
+     * {
+     *   "method": "ren_queryTxs",
+     *   "id": 1,
+     *   "jsonrpc": "2.0",
+     *   "params": {
+     *       "txStatus": "done",
+     *       "offset": "0",
+     *       "limit": "50"
+     *   }
+     * }
+     * 
+     * In particular, the following query will fetch the aforementioned transaction. 
+     * 
+     * ```
+     * POST https://lightnode-testnet.herokuapp.com/ HTTP/1.1
+     * Content-Type: application/json
+     * Accept: application/json
+     * 
+     * {
+     *   "method": "ren_queryTx",
+     *   "id": 1,
+     *   "jsonrpc": "2.0",
+     *   "params": {
+     *       "txHash": "8tmjw-pkNRPazHVzDArkJr62E_u1t6XVY6Zz-K2wzzk"
+     *   }
+     * }
+     * ```
+     * 
+     */
     it("should return hash transaction for BTC/toEthereum selector", () => {
         expect(
             toURLBase64(generateTransactionHash("1", "BTC/toEthereum",
